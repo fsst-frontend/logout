8040ccbb8a4f02c14a538539eff3d043b252702f
jason <test@163.com>
2026-06-12 15:28:47 +0800

    feat: 域名替换

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -54,7 +54,7 @@ async function requestHandler(config: InternalAxiosRequestConfig): Promise<Inter
   // todo keliang7 token 临时
   config.headers['x-request-id'] = savedDeviceId
   config.headers.Authorization = savedToken
-  config.headers['x-pkg-name'] = data?.packageName || 'com.fuyuan.lite'
+  config.headers['x-pkg-name'] = data?.packageName || 'com.glamextrade.lite'
   return config
 }
 
